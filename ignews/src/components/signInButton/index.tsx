57c314448521bf891aeec91d28a1d4be1c5a8804
BismarckOliveira <bismarckoliver@hotmail.com>
2021-05-06 19:38:59 -0300

    Finalizado autenticação com github

--- ignews/src/components/signInButton/index.tsx
+++ ignews/src/components/signInButton/index.tsx
@@ -1,20 +1,21 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn , useSession, signOut} from 'next-auth/client';
 
 export function SignInButton() {
-  const logged = true;
+  const [session]  = useSession();
 
+  console.log(session);
 
-
-  return logged ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button onClick={() => signOut()}  type="button" className={styles.signInButton}>
       <FaGithub color='#04D361'/>
-      tiagoluchtenberg
+      {session.user.name}
       <FiX className={styles.closeIcon}/> 
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button onClick={() => signIn('github')} type="button" className={styles.signInButton}>
       <FaGithub color='#EBA417' />
       Sign in with GitHub
     </button>
