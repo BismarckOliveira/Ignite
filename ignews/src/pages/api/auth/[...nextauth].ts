01433bd68f3b5fe9d859e548a408740d8f14d8d7
BismarckOliveira <bismarckoliver@hotmail.com>
2021-05-06 22:53:59 -0300

    Integrando aplicacao com faunaDB]

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { query as q } from 'faunadb'
+import { faunadb } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,11 +9,44 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user' 
+      scope: 'read:user'
     }),
     // ...add more providers here
   ],
 
-  // A database is optional, but required to persist accounts in a database
-  database: process.env.DATABASE_URL,
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await faunadb.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
+
+
 })
